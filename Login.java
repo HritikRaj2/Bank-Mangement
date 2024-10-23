61c59fdc9e84e42e9ab9fa12e8ff6f8262a33fe5
Hritik Raj <hritik23154049@akgec.ac.in>
2024-10-23 21:10:10 +0530

    login button

--- Login.java
+++ Login.java
@@ -1,13 +1,17 @@
 //package bank.management.system;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-public class Login extends JFrame{
+public class Login extends JFrame implements ActionListener{
+    JButton login,Signup,forget;
+    JTextField cardnotext,pintext;
     Login(){
         
         setTitle("ATM");
@@ -26,28 +30,42 @@ public class Login extends JFrame{
         cardno.setBounds(150, 70, 300, 150);
         cardno.setFont(new Font(getName(),ABORT,20));
         add(cardno);
-        JTextField cardnotext=new JTextField();
+        cardnotext=new JTextField();
         cardnotext.setBounds(300, 130, 300, 25);
         add(cardnotext);
         JLabel pin=new JLabel("PIN:");
         pin.setBounds(150, 100, 300, 150);
         pin.setFont(new Font(getName(), ABORT,20));
         add(pin);
-        JTextField pintext=new JTextField();
+        pintext=new JTextField();
         pintext.setBounds(300,165, 300, 25);
         add(pintext);
-        JButton login=new JButton("Login");
+        login=new JButton("Login");
         login.setBounds(350,195,150,25);
         add(login);
-        JButton Signup=new JButton("SIGNUP");
+        login.addActionListener(this);
+        Signup=new JButton("SIGNUP");
         Signup.setBounds(250,225,150,25);
         add(Signup);
-        JButton forget=new JButton("FORGET");
+        Signup.addActionListener(this);
+        forget=new JButton("FORGET");
         forget.setBounds(450,225,150,25);
         add(forget);
+        forget.addActionListener(this);
         setSize(800,480);
         setVisible(true);
         setLocation(350,200);
+    }
+    public void actionPerformed(ActionEvent ae){
+        if (ae.getSource()==login){
+
+        }
+        else if( ae.getSource()==Signup){
+
+        }
+        else if(ae.getSource()==forget){
+
+        }
     }
     public static void main(String args[]){
         new Login();
---
+++ signup.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+public class signup extends JFrame{
+    signup(){
+        
+        setSize(850,800);
+        setLocation(350,10);
+        setVisible(true);
+    }
+    public static void main(String args[]){
+        new signup();
+    }
+}
