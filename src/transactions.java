26cfd7172c9733bd9a6ba17acd626ad9a7c30120
Hritik Raj <hritik23154049@akgec.ac.in>
2024-12-03 17:49:29 +0530

    fix errors

--- src/transactions.java
+++ src/transactions.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,11 +16,18 @@ public class transactions extends JFrame implements ActionListener{
         Image i2=i1.getImage().getScaledInstance(850,800,Image.SCALE_DEFAULT);
         ImageIcon i3=new ImageIcon(i2);
         JLabel imglabel=new JLabel(i3);
-        imglabel.setBounds(0,10,850,800);
+        imglabel.setBounds(0,0,850,800);
         imglabel.setForeground(Color.WHITE);
         add(imglabel);
+        JLabel txt1=new JLabel("Please Select your Transaction");
+        txt1.setBounds(200,270,300,15);
+        txt1.setFont(new Font(getName(),Font.BOLD,15));
+        txt1.setForeground(Color.WHITE);
+        imglabel.add(txt1);
         JButton bt1=new JButton("Deposit");
-        bt1.setBounds(300,300,350,100);
+        bt1.setBounds(150,373,100,20);
+        imglabel.add(bt1);
+        //add new bt2 here
         setLayout(null);
         setSize(850,800);
         setLocation(350,10);
